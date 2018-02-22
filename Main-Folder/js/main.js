98e41d338300467e23d6b042fe7c31bdbc13d34e
Michael Peltzer <mwpeltzer@gmail.com>
2018-02-21 21:51:16 -0800

    added routes to various pages and corrected padding for home page picture

--- Main-Folder/js/main.js
+++ Main-Folder/js/main.js
@@ -12,15 +12,15 @@ function main() {
    /*====================================
     Main Navigation Stick to Top when Scroll
     ======================================*/
-    function sticky_relocate() {
-      var window_top = $(window).scrollTop();
-      var div_top = $('#sticky-anchor').offset().top;
-      if (window_top > div_top) {
-          $('#tf-menu').addClass('stick');
-      } else {
-          $('#tf-menu').removeClass('stick');
-      }
-  }
+  //   function sticky_relocate() {
+  //     var window_top = $(window).scrollTop();
+  //     var div_top = $('#sticky-anchor').offset().top;
+  //     if (window_top > div_top) {
+  //         $('#tf-menu').addClass('stick');
+  //     } else {
+  //         $('#tf-menu').removeClass('stick');
+  //     }
+  // }
 
   $(function () {
       $(window).scroll(sticky_relocate);
